27761e5dbae731beea573b1eba12030ee6c339ce
zaczig <beastzac@yahoo.com>
2023-08-06 01:43:59 -0500

    switched to XR Origin from other project

--- Assets/NetworkPlayerSpawner.cs
+++ Assets/NetworkPlayerSpawner.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+
+using Unity.XR.CoreUtils;
+
 public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
 {
     private GameObject spawnedPlayerPrefab;
@@ -16,4 +19,24 @@ public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
         base.OnLeftRoom();
         PhotonNetwork.Destroy(spawnedPlayerPrefab);
     }
+    public void DeactivatePlayer()
+    {
+        PhotonView player1 = PhotonView.Find(1001);
+        PhotonView player2 = PhotonView.Find(2001);
+
+        //head
+        player1.GetComponentInChildren<MeshRenderer>().enabled = false;
+        player2.GetComponentInChildren<MeshRenderer>().enabled = false;
+
+        //hands
+        SkinnedMeshRenderer[] p1hands = player1.GetComponentsInChildren<SkinnedMeshRenderer>();
+        SkinnedMeshRenderer[] p2hands = player2.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+        for (int i = 0; i < p1hands.Length; i++)
+        {
+            p1hands[i].enabled = false;
+            p2hands[i].enabled = false;
+        }
+    }
+
 }
